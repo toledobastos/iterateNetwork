dfa9d78be6da92316d17922dfd6473b715ddc311
toledobastos <toledobastos@gmail.com>
2015-11-25 16:45:02 -0800

    # housekeeping

--- R/plotEstimates.R
+++ R/plotEstimates.R
@@ -8,7 +8,8 @@ plotEstimates <- function(list.complete,
     if(return.estimates=="ALL") { return.estimates <- 1:ncol(list.complete[[1]]) }
     
     # define colors
-    if(col=="auto") { colorsmetric <- rainbow(length(list.complete)) }
+    if(col=="auto") { colorsmetric <- rainbow(length(list.complete)) } 
+    else { colorsmetric <- col }
     
     # prepare for plotting
     estimates.total <- length(return.estimates)-1
@@ -19,7 +20,7 @@ plotEstimates <- function(list.complete,
     if(estimates.count>500 && estimates.count<=1000) { lwd.by.iteration <- .7}
     if(estimates.count>1000 && estimates.count<=2000) { lwd.by.iteration <- .5}
     if(estimates.count>2000 && estimates.count<=4000) { lwd.by.iteration <- .3}
-    if(estimates.count>4000) { lwd.by.iteration <- 0.1}
+    if(estimates.count>4000) { lwd.by.iteration <- 0.1 }
     
     # set plot window
     if(estimates.total<6) { plot.panels <- c(estimates.total,1) }
@@ -33,7 +34,11 @@ plotEstimates <- function(list.complete,
     if(estimates.total==15) { plot.panels <- c(3,5) }
     if(estimates.total>16 && estimates.total<18) { plot.panels <- c(4,5) }
     if(estimates.total==18) { plot.panels <- c(3,6) }
-    if(estimates.total>18) { plot.panels <- c(5,5) }
+    if(estimates.total>18 && estimates.total<=20) { plot.panels <- c(4,5) }
+    if(estimates.total>20) { plot.panels <- c(round(sqrt(estimates.total)+.50),round(sqrt(estimates.total)+.50)) }
+    if(estimates.total==28) { plot.panels <- c(4,7) }
+    
+    # plot to file
     png(paste0("selected_network_estimates_complete_by_",tolower(attribute),".png"), type='cairo', width=plot.panels[2]*4,height=plot.panels[1]*4, units='in', res=200)
     par(mfrow=plot.panels, oma = c(4, 1, 1, 1))
     labels.plot1 <- 1:length(list.complete[[1]]$sample)
@@ -42,14 +47,13 @@ plotEstimates <- function(list.complete,
         plot(as.numeric(list.complete[[1]][,i]), xlab="", ylab="", col=NULL, cex=0.5, xaxt="n", main=paste(colnames(list.complete[[1]])[i]), type=plot.type, lwd=lwd.by.iteration,cex.lab=1.6, cex.axis=1.6, cex.main=2.5, cex.sub=2)
         for(u in 1:length(list.complete)) {
             if(plot.type=="l") { lines(as.numeric(list.complete[[u]][,i]), xlab="", ylab="", col=colorsmetric[u], cex=0.5, xaxt="n", lwd=lwd.by.iteration,cex.lab=1.6, cex.axis=1.6, cex.main=2.5, cex.sub=2, type="l") }
-            if(plot.type=="p") { points(as.numeric(list.complete[[u]][,i]), xlab="", ylab="", col=colorsmetric[u], cex=0.5, xaxt="n", lwd=lwd.by.iteration,cex.lab=1.6, cex.axis=1.6, cex.main=2.5, cex.sub=2, type="p") }
-            
+            if(plot.type=="p") { points(as.numeric(list.complete[[u]][,i]), xlab="", ylab="", col=colorsmetric[u], cex=0.5, xaxt="n", lwd=lwd.by.iteration,cex.lab=1.6, cex.axis=1.6, cex.main=2.5, cex.sub=2, type="p") }   
         }
         axis(1, at=labels.plot1, labels=labels.plot2)
     }
     par(fig = c(0, 1, 0, 1), oma = c(0, 0, 0, 0), mar = c(0, 0, 0, 0), new = TRUE)
     plot(0, 0, type = "n", bty = "n", xaxt = "n", yaxt = "n")
     legend("bottom", names(list.complete), xpd = TRUE, horiz = TRUE, inset = c(0, 0), bty="n", pch=19, col=colorsmetric, cex=3)
-    dev.off() 
+    dev.off()
     return(print(paste0("Iteration completed. Plot saved at ",getwd(),"/selected_network_estimates_complete_by_",tolower(attribute),".png")))
 }
